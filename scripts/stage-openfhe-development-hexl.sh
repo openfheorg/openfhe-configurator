ad774849ea1c58fd431f1ab725f5ea6d3044a952
Jack Bates <jbates@dualitytech.com>
2022-03-24 11:10:05 -0600

    bugfix: initial configure of hexl-enabled was failing

--- scripts/stage-openfhe-development-hexl.sh
+++ scripts/stage-openfhe-development-hexl.sh
@@ -7,7 +7,21 @@ ROOT=`pwd`
 
 $ROOT/scripts/stage-openfhe-development.sh || abort "Unable to stage openfhe-development [hexl-enabled]."
 
-cd $ROOT/repos/openfhe-hexl || abort "Unable to enter openfhe-hexl directory."
+if [ ! -d $ROOT/repos/openfhe-hexl ]; then
+  cd $ROOT/repos || abort "Unable to enter the repos directory."
+  git clone https://github.com/openfheorg/openfhe-hexl.git || abort "Unable to clone the openfhe-hexl repository."
+fi
+
+separator
+echo "Switching to branch openfhe-hexl $OPENFHE_HEXL_BRANCH."
+echo
+cd $ROOT/repos/openfhe-hexl || abort "Clone of openfhe-hexl failed."
+git checkout $OPENFHE_HEXL_BRANCH || abort "Checkout of openfhe-hexl branch $OPENFHE_HEXL_BRANCH failed."
+separator
+echo "Status of branch openfhe-hexl $OPENFHE_HEXL_BRANCH."
+echo
+git status
+
 scripts/stage-openfhe-development-hexl.sh || abort "Unable to stage openfhe-hexl into openfhe-development."
 
 separator
