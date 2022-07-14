9f183e4cf8ee69e3c03f8307703eecb817993b9e
Jack Bates <jbates@dualitytech.com>
2022-07-14 11:37:35 -0600

    changes for prebuilt HEXL

--- scripts/build-openfhe-development-hexl.sh
+++ scripts/build-openfhe-development-hexl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-CMAKE_FLAGS="-DWITH_INTEL_HEXL=ON -DWITH_NATIVEOPT=ON" ./scripts/build-openfhe-development.sh
+CMAKE_FLAGS="-DWITH_INTEL_HEXL=ON -DINTEL_HEXL_PREBUILT=ON -DWITH_NATIVEOPT=ON" ./scripts/build-openfhe-development.sh
